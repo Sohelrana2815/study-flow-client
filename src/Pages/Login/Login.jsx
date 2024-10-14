57181b50cc5e730144d39249a834243a0d2a2d3f
Sohelrana2815 <sohel152302@gmail.com>
2024-10-14 13:08:45 +0600

    retrive the value from login form

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,23 +1,39 @@
+import { useFormik } from "formik";
+
 const Login = () => {
-  const handleSubmit = (e) => {
-    e.preventDefault();
-  };
+  // Formik hook , initial form values and a submit function for
+  // grab form data when submitted
+
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    onSubmit: (values, { resetForm }) => {
+      console.log(values);
+      resetForm();
+    },
+  });
+
   return (
     <>
       <div className="flex items-center justify-center min-h-screen flex-col space-y-8">
         <h2 className="text-center font-serif text-purple-500 text-4xl">
-        REACT  FORMIK FORM
+          REACT FORMIK FORM
         </h2>
         <div className="w-full max-w-sm p-6 bg-white rounded-lg shadow-md">
           <h2 className="text-2xl font-bold text-center mb-6">Login</h2>
-          <form onSubmit={handleSubmit}>
+          <form onSubmit={formik.handleSubmit}>
             <div className="mb-4">
               <label htmlFor="email" className="block mb-1 font-medium">
                 Email
               </label>
               <input
                 type="email"
-                className="input bg-white input-primary w-full"
+                name="email"
+                onChange={formik.handleChange}
+                value={formik.values.email}
+                className="input text-purple-600 bg-white input-primary w-full"
                 required
               />
             </div>
@@ -27,7 +43,10 @@ const Login = () => {
               </label>
               <input
                 type="password"
-                className="input bg-white input-primary w-full"
+                name="password"
+                onChange={formik.handleChange}
+                value={formik.values.password}
+                className="input bg-white text-purple-600 input-primary w-full"
                 required
               />
             </div>
