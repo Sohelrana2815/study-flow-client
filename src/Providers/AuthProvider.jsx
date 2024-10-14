ee92a7b604394b7e448bb48c22d8c9975343800f
Sohelrana2815 <sohel152302@gmail.com>
2024-10-14 16:53:36 +0600

    set proptypes for AuthProvider component

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -1,21 +1,21 @@
 import { createContext } from "react";
+import PropTypes from "prop-types";
+export const AuthContext = createContext(null);
 
-export const AuthContext = createContext(null)
+const AuthProvider = ({ children }) => {
+  const authInformation = {
 
-const AuthProvider = ({children}) => {
+  };
 
-    
+  return (
+    <AuthContext.Provider value={authInformation}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
 
-    const authInformation = {
-     
-
-    }
-
-    return (
-        <AuthContext.Provider value={authInformation} >
-             {children}
-        </AuthContext.Provider>
-    );
+AuthProvider.propTypes = {
+  children: PropTypes.node.isRequired,
 };
 
 export default AuthProvider;
