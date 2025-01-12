ba14c1cbdc80d50b75373f5f4fb0d2db8b23613a
Sohelrana2815 <sohel152302@gmail.com>
2025-01-12 18:45:28 +0600

    added some photos for new section

---
+++ src/Pages/Home/Courses/Courses.jsx
@@ -0,0 +1,19 @@
+
+const Courses = () => {
+  return (
+    <>
+      <div>
+        <h2>Popular Online Courses</h2>
+        <p>
+          Rapidiously expedite granular imperatives before economically sound
+          web services. Credibly actualize pandemic strategic themeplatform.
+        </p>
+        <div>
+
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Courses;
